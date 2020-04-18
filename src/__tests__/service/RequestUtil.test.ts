ade8bd1fed389bd06c760da4d0f15b158e8e21f1
Nicholas Melnick <nick@abstractwankery.com>
2020-04-17 22:54:18 -0400

    Fix RequestUtil

---
+++ src/__tests__/service/RequestUtil.test.ts
@@ -0,0 +1,29 @@
+import * as RequestUtil from "../../service/RequestUtil";
+import { BaseLiveJournal } from "../../service/BaseLiveJournal";
+import { ChallengeResponse } from "../../dto/lj/ChallengeResponse";
+
+class TestLiveJournal extends BaseLiveJournal {
+    protected getUrl(): string {
+        return "";
+    }
+
+    public async getChallenge(): Promise<ChallengeResponse> {
+        return {
+            challenge: "testchallenge",
+            server_time: 0,
+            expire_time: 0
+        };
+    }
+}
+
+test("generateBaseRequest provides a ver and a clientversion", async () => {
+    const req = await RequestUtil.generateBaseRequest("abc123", new TestLiveJournal());
+    expect(req.ver).toEqual(1);
+    expect(req.clientversion).toBeDefined();
+});
+
+test("generateBaseRequest provides an auth_challenge and auth_response", async () => {
+    const req = await RequestUtil.generateBaseRequest("abc123", new TestLiveJournal());
+    expect(req.auth_challenge).toEqual("testchallenge");
+    expect(req.auth_response).toBeDefined();
+});
--- src/service/RequestUtil.ts
+++ src/service/RequestUtil.ts
@@ -2,14 +2,16 @@ import { createHash } from "crypto";
 
 import { IWithRequired } from "../dto/lj/IWithRequired";
 import { IWithChallenge } from "../dto/lj/IWithChallenge";
+import { BaseLiveJournal } from "./BaseLiveJournal";
 
 /**
  * Generate the base request for most XML-RPC calls, to be built upon for
  * creating a complete request.
  * @param hashedPassword User password, in MD5 hex
+ * @param lj Instance of a BaseLiveJournal implementation
  */
-export async function generateBaseRequest(hashedPassword: string): Promise<(IWithRequired & IWithChallenge)> {
-    const challenge = await generateChallenge(hashedPassword);
+export async function generateBaseRequest(hashedPassword: string, lj: BaseLiveJournal): Promise<(IWithRequired & IWithChallenge)> {
+    const challenge = await generateChallenge(hashedPassword, lj);
     return {...generateRequiredRequest(), ...challenge};
 }
 
@@ -20,8 +22,8 @@ function generateRequiredRequest(): IWithRequired {
     };
 }
 
-async function generateChallenge(hashedPassword: string): Promise<IWithChallenge> {
-    const r = await this.getChallenge();
+async function generateChallenge(hashedPassword: string, lj: BaseLiveJournal): Promise<IWithChallenge> {
+    const r = await lj.getChallenge();
     const challenge = r.challenge;
     const response = createHash('md5').update(challenge + hashedPassword).digest("hex");
     return {
